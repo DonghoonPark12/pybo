0c9cd4b6b1484f871964927449cc9520929c974d
donghoonpark12 <donghunp1212@gmail.com>
2025-04-01 19:35:20 +0900

    feat: add user login schema, API, verify process

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -1,9 +1,18 @@
+from datetime import datetime, timedelta
+
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 from sqlalchemy.orm import Session
 from starlette import status
 
 from database import get_db
 from domain.user import user_schema, user_crud
+from domain.user.user_crud import pwd_context
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+SECRET_KEY = "4ab2fce7a6bd79e1c014396315ed322dd6edb1c5d975c6b74a2904135172c03c"
+ALGORITHM = "HS256"
 
 router = APIRouter(
     prefix="/api/user",
@@ -16,3 +25,38 @@ def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail="이미 존재하는 사용자입니다.")
     user_crud.create_user(db=db, user_create=_user_create)
+
+@router.post("/login", response_model=user_schema.Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+        입력 받은 form_data.password와 DB에 저장된 user.password 가 일치하는지 조사한다.
+        이때 회원가입시 사용되었던 pwd_context가 사용된다.
+        pwd_context의 verify함수는 암호화 되지 않은 비밀번호를 암호화하여 데이터베이스에 저장된 암호와 일치하는지 판단한다.
+        일치하지 않는다면, 사용자 인증오류인 401 오류를 리터한다.
+
+        jwt(Json Web Token)를 사용하여 액세스 토큰을 생성한다.
+            - jwt란 Json 포맷을 이용하여 사용자에 대한 속성을 저장하는 Claim 기반의 Web Token이다.
+            - https://jwt.io/
+    """
+    # check user and password
+    user = user_crud.get_user(db, form_data.username)
+    if not user or not pwd_context.verify(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # make access token
+    data = {
+        "sub": user.username,
+        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    }
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "username": user.username
+    }
+
