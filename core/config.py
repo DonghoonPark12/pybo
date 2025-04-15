dbac65162364ab9744fec615774d683d1591844b
donghoonpark12 <donghunp1212@gmail.com>
2025-04-15 13:12:28 +0900

    chore: update frontend

--- core/config.py
+++ core/config.py
@@ -1,11 +1,12 @@
+import os
 from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), "../.env"))
 
-    database_url: str = "mysql+pymysql://root:Chatbot1!@localhost:3306/pybo"
+    database_url: str = ""
 
 @lru_cache
 def get_settings() -> Settings:
